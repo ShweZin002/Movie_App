73223921110728fd94a8b91d21ddcf0b2588455c
ShweZin002 <shwezin56824@gmail.com>
2025-01-28 18:43:10 +0630

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
      <Header/>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/Movie_App" element={<Home/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/movies/detail/:movieId" element={<Details/>} />
 
